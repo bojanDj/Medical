cb8efdf20cbf54f3c05cfe8cf9d0acd4674cb3ea
bojanDj <bojan.djekic97@yahoo.com>
2021-04-15 23:29:50 +0200

    Initial commit

---
+++ medic/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to medic
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ medic/test/medic/core_test.clj
@@ -0,0 +1,7 @@
+(ns medic.core-test
+  (:require [clojure.test :refer :all]
+            [medic.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
